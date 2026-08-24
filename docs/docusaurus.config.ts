797ee8f116ff9c94d15efd45f05d22057e57ea1f
Intellinfra-Dev <dev@lithastra.com>
2026-08-24 18:10:45 +0900

    docs(docs): label current docs as v1.5.2 (#22)
    Signed-off-by: dev lithastra <dev@lithastra.com>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -41,7 +41,7 @@ const config: Config = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: '1.5.1',
+              label: '1.5.2',
             },
           },
         },
